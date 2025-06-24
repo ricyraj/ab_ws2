7bc4b2b6631c046f74a8c1d38640ff0da3d890b1
ricyraj <ricyraj@gmail.com>
2025-06-25 01:02:12 +0530

    Create aflplugin.h

---
+++ include/aflplugin.h
@@ -0,0 +1,30 @@
+#ifndef AFLPLUGIN_H
+#define AFLPLUGIN_H
+
+#define PLUGINAPI extern "C" __declspec(dllexport)
+#define PLUGIN_OK 0
+
+typedef struct {
+    char Name[64];
+    char Vendor[64];
+} PluginInfo;
+
+typedef struct {
+    int version;
+    int reserved;
+} RTPluginInfo;
+
+typedef struct {
+    char symbol[32];
+    float last;
+    float bid;
+    float ask;
+    float open;
+    float high;
+    float low;
+    float close;
+    float volume;
+    unsigned int datetime;
+} RTQuote;
+
+#endif // AFLPLUGIN_H
